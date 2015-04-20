fdcd1d1218bc49bc4d199c1bf2301bef549162d9
Fully34 <cfullinw34@gmail.com>
2015-04-20 13:29:16 -0600

    have one reverse increment, need to force the increment to persis until next instance of count%7==0

--- counting_game.rb
+++ counting_game.rb
@@ -7,11 +7,18 @@ def counting_game(num)
 	count = 0
 
 	player = 0
+
 	rev = false
 	#skip = false
 
 
 	while count < num
+		if (count%7 == 0)
+			rev = true
+			count +=1
+			player -= 1
+			game << [count,player]
+		end
 		count +=1
 		player +=1
 		game << [count,player]
